6855dd1e6ba761d81a503075d7b17e3f5ed4e60b
Will <will@forwardbounding.com>
2025-03-24 09:32:13 -0400

    v0.18.4 bottle info

--- Formula/ra-aid.rb
+++ Formula/ra-aid.rb
@@ -18,6 +18,12 @@ class RaAid < Formula
   depends_on "python@3.12"
   depends_on "ripgrep"
 
+  bottle do
+    rebuild 1
+    root_url "https://github.com/ai-christianson/homebrew-ra-aid/releases/download/ra-aid-0.18.4"
+    sha256 cellar: :any, arm64_sequoia: "680741acdb00572e3a70b5513bcda7bc39c0ec6e0141fd71070d3cd1be135bf1"
+  end
+
   resource "aiohappyeyeballs" do
     url "https://files.pythonhosted.org/packages/26/30/f84a107a9c4331c14b2b586036f40965c128aa4fee4dda5d3d51cb14ad54/aiohappyeyeballs-2.6.1.tar.gz"
     sha256 "c3f9d0113123803ccadfdf3f0faa505bc78e6a72d1cc4806cbd719826e943558"
